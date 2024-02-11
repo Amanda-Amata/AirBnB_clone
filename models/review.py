453c70c6ec5acb9c678285a2d3ddc37989759d8e
Amanda Mabunda <amandamabu14@gmail.com>
2024-02-11 05:24:13 -0800

    errors fixed

--- models/review.py
+++ models/review.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines the review class"""
-from models.base_models import BaseModel
+from models.base_model import BaseModel
 
 class Review(BaseModel):
     """represents a review 
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -10,7 +10,7 @@ import models
 import unittest
 from datetime import datetime
 from time import sleep
-from models.user import user
+from models.user import User
 
 
 class TestUser_instantiation(unittest.TestCase):
